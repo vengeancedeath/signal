51c0b223da05c12a4489a73615360f3b2cac5ed1
revenges <swp18258685707@outlook.com>
2021-06-02 21:43:49 +0800

    master

--- signal_2.py
+++ signal_2.py
@@ -1,20 +1,78 @@
-# -*- coding: UTF-8 -*-
-#coding=utf-8
+from thinkdsp import CosSignal, SinSignal, decorate, read_wave
 import matplotlib.pyplot as plt
-import numpy as np 
-# linspace 第一个参数序列起始值, 第二个参数序列结束值,第三个参数为样本数默认50
-x = np.linspace(0, 3 * np.pi, 100)#范围0到3pi
-y = np.sin(x)
-plt.rcParams['font.sans-serif']=['SimHei'] #加上这一句就能在图表中显示中文
-plt.rcParams['axes.unicode_minus']=False #用来正常显示负号
-plt.subplot(1,2,1)
-plt.title(r'$f(x)=sin(x)$') 
-plt.plot(x, y)
+# wave = read_wave(r'D:\vs_code_prace\Test\dublie_trumpet.wav')
+# wave.normalize()
+# wave.make_audio()
+# wave.plot()
+# plt.show()
+# segment = wave.segment(start=1.1, duration=0.3)
+# segment.make_audio()
+# segment.plot()
+# plt.show()
+# segment.segment(start=1.1, duration=0.005).plot()
+# spectrum = segment.make_spectrum()
+# spectrum.plot(high=7000)
+# plt.show()
+# spectrum.low_pass(4000)
+# spectrum.plot(high=7000)
+# plt.show()
+# spectrum.high_pass(1000)
+# spectrum.plot(high=7000)
+# plt.show()
+# spectrum.band_stop(2500,1000)
+# spectrum.plot(high=7000)
+# plt.show()
 
-x1 = [t*0.375*np.pi for t in x] #x序列的每一个数放入t的表达式中
-y1 = np.sin(x1)
-plt.subplot(1,2,2)
-# plt.title(u"测试2") #注意：在前面加一个u
-plt.title(r'$f(x)=sin(\omega x), \omega = \frac{3}{8} \pi$') 
-plt.plot(x1, y1)
+# cos_sig = CosSignal(freq=440,amp=1.0,offset=0)
+# sin_sig = SinSignal(freq=880,amp=2.0,offset=0)
+# # 正弦信号和波形
+# plt.subplot(231)
+# cos_sig.plot()
+# decorate(xlabel='Time (s)')
+
+# wave1 = cos_sig.make_wave(duration=1)
+# wave1.apodize()
+# wave1.make_audio()
+# spectrum1 = wave1.make_spectrum()
+# plt.subplot(232)
+# spectrum1.plot(high=2000)
+
+# # 余弦信号和波形
+# plt.subplot(233)
+# sin_sig.plot()
+# decorate(xlabel='Time (s)')
+
+
+# wave2 = cos_sig.make_wave(duration=1)
+# wave2.apodize()
+# wave2.make_audio()
+# spectrum2 = wave2.make_spectrum()
+# plt.subplot(234)
+# spectrum2.plot(high=2000)
+
+# # 混合叠加的信号和波形
+# mix = sin_sig + cos_sig
+# plt.subplot(235)
+# decorate(xlabel='Time (s)')
+# mix.plot()
+
+# wave3 = mix.make_wave(duration=1)
+# wave3.apodize()
+# wave3.make_audio()
+# spectrum3 = wave3.make_spectrum()
+# plt.subplot(236)
+# spectrum3.plot(high=2000)
+# plt.show()
+
+wave4 = read_wave(r'D:\vs_code_prace\Test\dublie_trumpet.wav')
+wave4.normalize()
+wave4.make_audio()
+
+
+def stretch(wave,factor):
+    wave.ts *= factor
+    wave.framerate /= factor
+stretch(wave4, 0.5)
+
+wave4.plot()
 plt.show()
